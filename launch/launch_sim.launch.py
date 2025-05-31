9e42cbd73a1e74efc95e99de9d9978982b04ce69
Angeraa <andrew.nicholas.gera@gmail.com>
2025-05-31 09:41:26 -0400

    formatting

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -13,24 +13,24 @@ def generate_launch_description():
   package_name='mole'
 
   rsp = IncludeLaunchDescription(
-              PythonLaunchDescriptionSource([os.path.join(
-                  get_package_share_directory(package_name),'launch','rsp.launch.py'
-              )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+          PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+          )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
   )
 
   default_world = os.path.join(
-      get_package_share_directory(package_name),
-      'worlds',
-      'empty.world'
-      )    
+    get_package_share_directory(package_name),
+    'worlds',
+    'empty.world'
+  )    
   
   world = LaunchConfiguration('world')
 
   world_arg = DeclareLaunchArgument(
-      'world',
-      default_value=default_world,
-      description='World to load'
-      )
+    'world',
+    default_value=default_world,
+    description='World to load'
+  )
 
   # Include the Gazebo launch file, provided by the ros_gz_sim package
   gazebo = IncludeLaunchDescription(
@@ -39,7 +39,7 @@ def generate_launch_description():
                   launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
             )
 
-  # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+  # Run the spawner node from the ros_gz_sim package
   spawn_entity = Node(package='ros_gz_sim', executable='create',
                       arguments=['-topic', '/robot_description',
                                   '-name', 'mole',
@@ -48,27 +48,27 @@ def generate_launch_description():
 
 
   diff_drive_spawner = Node(
-      package="controller_manager",
-      executable="spawner",
-      arguments=["diff_controller"],
+    package="controller_manager",
+    executable="spawner",
+    arguments=["diff_controller"],
   )
 
   joint_broad_spawner = Node(
-      package="controller_manager",
-      executable="spawner",
-      arguments=["joint_broadcaster"],
+    package="controller_manager",
+    executable="spawner",
+    arguments=["joint_broadcaster"],
   )
 
 
   bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
   ros_gz_bridge = Node(
-      package="ros_gz_bridge",
-      executable="parameter_bridge",
-      arguments=[
-          '--ros-args',
-          '-p',
-          f'config_file:={bridge_params}',
-      ]
+    package="ros_gz_bridge",
+    executable="parameter_bridge",
+    arguments=[
+      '--ros-args',
+      '-p',
+      f'config_file:={bridge_params}',
+    ]
   )
 
   twist_stamper = Node(
@@ -79,14 +79,14 @@ def generate_launch_description():
                 ("/cmd_vel_out", "/diff_controller/cmd_vel")]
   )
 
-  # Launch them all!
+  # Launch
   return LaunchDescription([
-      rsp,
-      world_arg,
-      gazebo,
-      ros_gz_bridge,
-      spawn_entity,
-      diff_drive_spawner,
-      joint_broad_spawner,
-      twist_stamper,
+    rsp,
+    world_arg,
+    gazebo,
+    ros_gz_bridge,
+    spawn_entity,
+    diff_drive_spawner,
+    joint_broad_spawner,
+    twist_stamper,
   ])
